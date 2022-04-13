a95cb63d192d8844895ad3c8c107c7abe85a282f
Ski321 <103225231+Ski321@users.noreply.github.com>
2022-04-12 23:44:31 -0600

    asdf

---
+++ testeee.js
@@ -0,0 +1,9 @@
+(function() { 
+  var div1 = document.createElement("div");
+  div1.id = "z1z1"  
+  div1.style.position = "absolute";
+  div1.style.top = "50%";
+  div1.style.left = "50%";
+  div1.style.margin = '0px';
+  div1.style.transform = 'translate(-50%, -50%)'
+})();
