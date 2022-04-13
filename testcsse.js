92373a210a825588fd1c3bc2cf29b39788fd0faa
Ski321 <103225231+Ski321@users.noreply.github.com>
2022-04-12 23:43:08 -0600

    asdf

--- testcsse.js
+++ testcsse.js
@@ -1,7 +1,9 @@
 (function() { 
-GM_addStyle(
-  body {
-    background-color: red;
-  }
-)
+  var div1 = document.createElement("div");
+  div1.id = "z1z1"  
+  div1.style.position = "absolute";
+  div1.style.top = "50%";
+  div1.style.left = "50%";
+  div1.style.margin = '0px';
+  div1.style.transform = 'translate(-50%, -50%)'
 })();
